8c19c4386c63c8f5aadd5346dfc4273aa02866fa
andersonmerten <anderson.merten@gmail.com>
2017-10-13 16:55:27 -0300

    melhoria no layout de detalhes

--- controle_estoque/ControleEstoque/Models/Cliente.cs
+++ controle_estoque/ControleEstoque/Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +10,8 @@ namespace ControleEstoque.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public string Fone { get; set; }
+            //[StringLength(10, ErrorMessage = "Name cannot be longer than 10 characters.")]
+            public string Fone { get; set; }
         public string Cpf { get; set; }
 
     }
