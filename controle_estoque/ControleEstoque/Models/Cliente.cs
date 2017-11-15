f07634785bd3ad2c1eb0149e778a0fc98093be16
andersonmerten <anderson.merten@gmail.com>
2017-11-14 23:30:11 -0200

    validação de data com bug

--- controle_estoque/ControleEstoque/Models/Cliente.cs
+++ controle_estoque/ControleEstoque/Models/Cliente.cs
@@ -16,16 +16,17 @@ namespace ControleEstoque.Models
             public string Nome { get; set; }
 
             [Required]
-            [StringLength(12, ErrorMessage ="O número do telefone não pode ter mais que 12 caracteres!")]
+            [StringLength(12, ErrorMessage = "O número do telefone não pode ter mais que 12 caracteres!")]
             public string Fone { get; set; }
             [Required]
-            [StringLength(11,ErrorMessage ="O CPF deve conter apenas os 11 números!")]
+            [StringLength(11, ErrorMessage = "O CPF deve conter apenas os 11 números!")]
             public string Cpf { get; set; }
             [Required(ErrorMessage = "Digite o valor do e-mail!")]
             [EmailAddress(ErrorMessage = "Digite um e-mail válido")]
             public string Email { get; set; }
             [Required]
-            public DateTime Nascimento { get; set; }            
+            
+            public DateTime? Nascimento { get; set; }            
             [Required]
             public String Imagem { get; set; }
 
